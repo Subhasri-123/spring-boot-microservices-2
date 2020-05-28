59bcd346f085f8f9d0b5caac0fb05ef6aeadec83
subhasri_sahoo <subhasri_sahoo@THBSGVLPT-0498.thbs.india.com>
2020-05-28 09:21:09 -0600

    New Rating

--- src/main/java/com/example/demo/controller/RatingController.java
+++ src/main/java/com/example/demo/controller/RatingController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Rating;
+import com.example.demo.model.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -14,5 +18,16 @@ public class RatingController {
 	@GetMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
+	}
+	
+	@GetMapping("/users/{userId}")
+	public UserRating getAllRatings(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 5)
+				);
+		UserRating userRating = new UserRating();
+		userRating.setUserRatings(ratings);
+		return userRating;
 	}
 }
